3427228900d010822c0f35cd4cc45234c102d782
Andrew Drake <andrew@venturedrake.com>
2026-07-16 00:21:25 +1000

    feat: [US-002] - Add backToIndexAction() factory to EmailCampaignResource

--- src/Resources/EmailCampaigns/EmailCampaignResource.php
+++ src/Resources/EmailCampaigns/EmailCampaignResource.php
@@ -161,6 +161,15 @@ class EmailCampaignResource extends Resource
         ];
     }
 
+    public static function backToIndexAction(): Actions\Action
+    {
+        return Actions\Action::make('backToIndex')
+            ->label(__('laravel-crm-filament::labels.actions.back_to_email_campaigns'))
+            ->icon('heroicon-o-arrow-left')
+            ->color('gray')
+            ->url(static::getUrl('index'));
+    }
+
     public static function getPages(): array
     {
         return [
