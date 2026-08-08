66d8a4689f8b0b91461d162f3fbfc6b48398a554
mohammed ayesh <ayeshm447@gmail.com>
2026-08-08 08:21:22 +0300

    fix(tabs): prioritize ListRecords\Tab in CrmTab helper to resolve container initialization and builder errors on Filament v4/v5

--- src/Support/CrmTab.php
+++ src/Support/CrmTab.php
@@ -6,12 +6,16 @@ class CrmTab
 {
     public static function make(string $label): mixed
     {
-        if (class_exists(\Filament\Resources\Components\Tab::class)) {
-            return \Filament\Resources\Components\Tab::make($label);
-        }
-
         if (class_exists(\Filament\Resources\Pages\ListRecords\Tab::class)) {
             return \Filament\Resources\Pages\ListRecords\Tab::make($label);
+        }
+
+        if (class_exists(\Filament\Tables\Tabs\Tab::class)) {
+            return \Filament\Tables\Tabs\Tab::make($label);
+        }
+
+        if (class_exists(\Filament\Resources\Components\Tab::class)) {
+            return \Filament\Resources\Components\Tab::make($label);
         }
 
         return \Filament\Schemas\Components\Tabs\Tab::make($label);
